45fcc7322446af3ef7ad0e1dd540dfd91ba11faa
Alexandre Del Vecchio <alexandre.del-vecchio@digitick.com>
2018-12-27 14:11:45 +0100

    Fix : Redindent

--- 7/ex7_other.rb
+++ 7/ex7_other.rb
@@ -2,38 +2,39 @@ require '../libs/common'
 require "set"
 
 module Adventofcode
-    module FileParser
-        include Adventofcode::StreamFile
-        def parse(file)
-            tree = Hash.new { |h,k| h[k] = Set.new }
-            readFullContent(file).split("\n").map do |line|
-                matching = /^Step (\w).+step (\w).+$/.match(line.chomp)
-                tree[matching[2]].add(matching[1])
-            end
-            finalString = Set.new
-            remaining = tree.map { |k,v| [k, v.to_a] }.flatten.uniq.sort.to_set
+  module FileParser
+    include Adventofcode::StreamFile
 
-            until remaining.empty?
-                step = remaining.find {|step|
-                    # p step, tree[step], finalString
-                    # p "--------------"
-                    tree[step].subset?(finalString)
-                }
-                # p "Add #{step}"
-                remaining.delete(step)
-                finalString.add(step)
-            end
-            finalString
-        end
+    def parse(file)
+      tree = Hash.new {|h, k| h[k] = Set.new}
+      readFullContent(file).split("\n").map do |line|
+        matching = /^Step (\w).+step (\w).+$/.match(line.chomp)
+        tree[matching[2]].add(matching[1])
+      end
+      finalString = Set.new
+      remaining = tree.map {|k, v| [k, v.to_a]}.flatten.uniq.sort.to_set
+
+      until remaining.empty?
+        step = remaining.find {|step|
+          # p step, tree[step], finalString
+          # p "--------------"
+          tree[step].subset?(finalString)
+        }
+        # p "Add #{step}"
+        remaining.delete(step)
+        finalString.add(step)
+      end
+      finalString
     end
+  end
 end
 
 class PartOne
-    include Adventofcode::FileParser
+  include Adventofcode::FileParser
 end
 
 class PartTwo
-    include Adventofcode::FileParser
+  include Adventofcode::FileParser
 end
 
 # Exercice 7 Part One
