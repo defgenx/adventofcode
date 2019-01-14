7adf7978f19a921ec6e0d0b973871a41349da8cc
Alexandre Del Vecchio <alexandre.del-vecchio@digitick.com>
2019-01-14 12:26:33 +0100

    Ex4

---
+++ 2015/4/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    part_one();
+    part_two();
+}
+
+fn part_one() {
+    let reader = advent_of_code::file::read();
+    let mut string_hash = String::from("");
+    let string_ex = String::from(reader);
+    let mut counter = 0;
+    while !string_hash.starts_with("00000") {
+        let concat = vec![string_ex.clone(), counter.to_string()].join("");
+        string_hash = format!("{:x}", md5::compute(concat.clone()));
+//        println!("{:#?}", md5::compute(format!("b\"{}\"", concat)));
+        counter += 1;
+    }
+    println!("String {} has hash {} so response is {:#?}", string_ex, string_hash, (counter -1));
+}
+
+fn part_two() {
+    let reader = advent_of_code::file::read();
+    let mut string_hash = String::from("");
+    let string_ex = String::from(reader);
+    let mut counter = 0;
+    while !string_hash.starts_with("000000") {
+        let concat = vec![string_ex.clone(), counter.to_string()].join("");
+        string_hash = format!("{:x}", md5::compute(concat.clone()));
+//        println!("{:#?}", md5::compute(format!("b\"{}\"", concat)));
+        counter += 1;
+    }
+    println!("String {} has hash {} so response is {:#?}", string_ex, string_hash, (counter -1));
+}
