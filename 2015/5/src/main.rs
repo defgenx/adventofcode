daf187601ef86c20c872d8fc12501ac86c4c40c3
Alexandre Del Vecchio <alexandre.del-vecchio@digitick.com>
2019-01-16 18:26:08 +0100

    Ex5 2015

---
+++ 2015/5/src/main.rs
@@ -0,0 +1,44 @@
+fn main() {
+    part_one();
+}
+
+fn part_one() {
+    let reader = advent_of_code::file::read_stream();
+    let mut counter = 0;
+    for line in reader {
+        let my_line = line.unwrap();
+        if count_vowels(my_line.to_owned()) >= 3 && find_double(my_line.to_owned()) && contain_pattern(my_line.to_owned()){
+            counter += 1;
+        }
+    }
+
+    println!("{}", counter);
+}
+
+fn count_vowels(line: String) -> i32 {
+    let sum = line.chars().fold(0, |sum, x| {
+        if x == 'a' ||x == 'e' || x == 'i' || x == 'o' || x == 'u' {
+            sum + 1
+        } else {
+            sum
+        }
+    });
+    sum
+}
+
+fn find_double(line: String) -> bool {
+    let mut current_char: char = ' ';
+    let mut found = false;
+    for chara in line.chars() {
+        if current_char == chara {
+            found = true;
+            break;
+        }
+        current_char = chara;
+    }
+    found
+}
+
+fn contain_pattern(line: String) -> bool {
+    !line.contains("ab") && !line.contains("cd") && !line.contains("pq") && !line.contains("xy")
+}
