245f8ad8afe1acd9b0b347ad8e977f7a808afc34
Alexandre Del Vecchio <alexandre@buyco.co>
2021-12-06 17:53:40 +0100

    AoC 2021 Ex4

---
+++ 2021/4/b/b.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"2021/common"
+	"github.com/thoas/go-funk"
+	"log"
+	"strconv"
+	"strings"
+)
+
+type Value struct {
+	Number string
+	Found  bool
+}
+
+type Grids [][][]*Value
+
+var grids Grids
+
+func main() {
+	file := common.OpenInputFile("4/input.txt")
+	var nbrs = make([]string, 0)
+	var row = 0
+	var gridNumber = 0
+	var gridLineRow = 0
+	var currentGrid [][]*Value
+	buffer := common.ReadBuffer(file)
+	for buffer.Scan() {
+		rowtext := buffer.Text()
+		if row == 0 {
+			splittedVals := strings.Split(rowtext, ",")
+			nbrs = splittedVals
+			row++
+			continue
+		}
+		if rowtext == "" {
+			addGrid(gridNumber, currentGrid)
+			if currentGrid != nil {
+				gridNumber++
+				currentGrid = make([][]*Value, 0)
+			}
+			row = 2
+			gridLineRow = 0
+			continue
+		}
+		splittedVals := strings.Split(rowtext, " ")
+		var currentRow []*Value
+		for _, val := range splittedVals {
+			if val == "" {
+				continue
+			}
+			currentRow = append(currentRow, &Value{
+				Number: val,
+				Found:  false,
+			})
+		}
+		currentGrid = append(currentGrid, nil)
+		currentGrid[gridLineRow] = currentRow
+		gridLineRow++
+	}
+
+	addGrid(gridNumber, currentGrid)
+	completedGridNumber, calledNumber := findLastCompletedGrid(nbrs)
+	sum := 0
+	for _, col := range grids[completedGridNumber] {
+		for _, val := range col {
+			if !val.Found {
+				intVal, _ := strconv.Atoi(val.Number)
+				sum += intVal
+			}
+		}
+	}
+	log.Print("Grid ", completedGridNumber, " with ", sum, "=>", calledNumber)
+	calledNumberInt, _ := strconv.Atoi(calledNumber)
+	log.Printf("Val is %d", calledNumberInt*sum)
+}
+
+func addGrid(pos int, grid [][]*Value) {
+	if grid != nil {
+		grids = append(grids, nil)
+		grids[pos] = grid
+	}
+}
+
+func findLastCompletedGrid(numbers []string) (lastGrid int, lastNumber string) {
+	var tmpRow = make([][5]int, len(grids))
+	var tmpCol = make([][5]int, len(grids))
+	var wonGrids []int
+	for _, number := range numbers {
+		for gridIndex, grid := range grids {
+			if len(wonGrids) == len(grids) {
+				return
+			}
+			if funk.ContainsInt(wonGrids, gridIndex) {
+				continue
+			}
+		breakLoop:
+			for colIndex := 0; colIndex < 5; colIndex++ {
+				for rowIndex := 0; rowIndex < 5; rowIndex++ {
+					if grid[colIndex][rowIndex].Number == number {
+						grid[colIndex][rowIndex].Found = true
+						tmpRow[gridIndex][rowIndex]++
+					}
+					if grid[rowIndex][colIndex].Number == number {
+						grid[rowIndex][colIndex].Found = true
+						tmpCol[gridIndex][rowIndex]++
+					}
+					if tmpCol[gridIndex][rowIndex] == 5 || tmpRow[gridIndex][rowIndex] == 5 {
+						lastGrid = gridIndex
+						lastNumber = number
+						wonGrids = append(wonGrids, gridIndex)
+						break breakLoop
+					}
+				}
+
+			}
+		}
+	}
+	return
+}
--- 2021/go.mod
+++ 2021/go.mod
@@ -2,4 +2,6 @@ module 2021
 
 go 1.17
 
-require github.com/juliangruber/go-intersect v1.1.0 // indirect
+require github.com/juliangruber/go-intersect v1.1.0
+
+require github.com/thoas/go-funk v0.9.1
